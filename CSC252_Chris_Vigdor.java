4e1bdfd8e85949378c408b291a1d4d6aa90eb6c8
ChrisVigdor <chris.vigdor@gmail.com>
2022-10-09 09:57:13 -0400

    added file support
    added the ability to read input from a text file

--- CSC252_Chris_Vigdor.java
+++ CSC252_Chris_Vigdor.java
@@ -1,55 +1,67 @@
 /**
 Chris Vigdor
-09/13/2022
+10/09/2022
 Demo Program
 This porgram uses the Policy class i created to make an object to determine the insurance policy cost for a customer
+Expanded from part 1, now the program gets input from a text file
 */
 
 import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
 
 public class CSC252_Chris_Vigdor{
 
-   public static void main(String[] args){
+   public static void main(String[] args) throws IOException{
+   
+      int  policyHolderAge;
+      String providerName, policyFirstName, policyLastName, policyNumber, policySmokingStatus;
+      double policyHeightIn, policyWeightLbs;
+      
       Policy obj = new Policy();
+      ArrayList<Policy> policyList = new ArrayList<>();
+      File file = new File("PolicyInformation.txt");
+      Scanner inputFile = new Scanner(file);
       
-      Scanner keyboard = new Scanner(System.in);
+      while(inputFile.hasNext()) 
+      {
+         /**
+            Keeps thwrowing mismatch exception, not sure why when ive checked the inputs from the file and they are correct
+            if all vars are String it can read all data from file perfectly
+         */
+        policyNumber = inputFile.nextLine();
+        providerName = inputFile.nextLine();
+        policyFirstName = inputFile.nextLine();
+        policyLastName = inputFile.nextLine();
+        policyHolderAge = inputFile.nextInt();
+        policySmokingStatus = inputFile.nextLine();
+        policyHeightIn = inputFile.nextDouble();
+        policyWeightLbs = inputFile.nextDouble();
+        inputFile.nextLine();
+        
+                
+        
+         
+         
+        //policyList.add(new Policy (policyNumber, providerName, policyFirstName, policyLastName, policyHolderAge, policySmokingStatus, policyHeightIn, policyWeightLbs));
+      }
       
-      System.out.print("Please enter the Policy Number: ");
-      obj.setPolicyNum(keyboard.nextInt());
-      keyboard.nextLine();
       
-      System.out.print("Please enter the Provider Name: ");
-      obj.setProviderName(keyboard.nextLine());
       
-      System.out.print("Please enther the Policyholder's First Name: ");
-      obj.setPolicyFName(keyboard.nextLine());
       
-      System.out.print("Please enter the Policyholder's Last Name: ");
-      obj.setPolicyLName(keyboard.nextLine());
-      
-      System.out.print("Please enter the Policyholder's Age: ");
-      obj.setPolicyAge(keyboard.nextInt());
-      keyboard.nextLine();
-      
-      System.out.print("Please enter the Policyholer's Smokin Status(smoker/non-smoker): ");
-      obj.setSmokingStatus(keyboard.nextLine());
-      
-      System.out.print("Please enter the Policyholder's Height(in inches): ");
-      obj.setPolicyHeight(keyboard.nextDouble());
-      keyboard.nextLine();
-      
-      System.out.print("Please enter the Policyholder's Weight(in pounds): ");
-      obj.setPolicyWeight(keyboard.nextDouble());
-      
-      System.out.println("Policy number: "+ obj.getPolicyNum());
-      System.out.println("Provider Name: "+ obj.getProviderName());
-      System.out.println("Policyholder's First Name: "+ obj.getPolicyFName());
-      System.out.println("Policyholder's Last Name: "+ obj.getPolicyLName());
-      System.out.println("Policyholder's Age: "+ obj.getPolicyAge());
-      System.out.println("Policyholder's Smoking Status: " + obj.getSmokingStatus());
-      System.out.println("Policyholder's Height: "+ obj.getPolicyHeight() + " inches");
-      System.out.println("Policyholder's Weight: "+ obj.getPolicyWeight() + " ponds");
-      System.out.printf("Policyholder's BMI: %.2f\n",  obj.policyBMI());
-      System.out.printf("Policy Price: $%,.2f\n",  obj.calculateInsuranceCost());
+      for(int i = 0; i < policyList.size(); i++)
+         {  
+            Policy list = policyList.get(i);
+            System.out.println("1Policy number: "+ obj.getPolicyNum());
+            System.out.println("1Provider Name: "+ obj.getProviderName());
+            System.out.println("1Policyholder's First Name: "+ obj.getPolicyFName());
+            System.out.println("1Policyholder's Last Name: "+ obj.getPolicyLName());
+            System.out.println("1Policyholder's Age: "+ obj.getPolicyAge());
+            System.out.println("1Policyholder's Smoking Status: " + obj.getSmokingStatus());
+            System.out.println("1Policyholder's Height: "+ obj.getPolicyHeight() + " inches");
+            System.out.println("1Policyholder's Weight: "+ obj.getPolicyWeight() + " ponds");
+            System.out.printf("1Policyholder's BMI: %.2f\n",  obj.policyBMI());
+            System.out.printf("1Policy Price: $%,.2f\n",  obj.calculateInsuranceCost());
+         }
    }
 }
